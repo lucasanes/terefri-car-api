d859ac99314fa39e922b648a7e78bbcbb39d12b5
SkyLuke11 <sky.lucas003@gmail.com>
2024-08-07 21:39:27 -0300

    first

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = {
+  email: {
+    config: {
+      provider: 'sendmail',
+      providerOptions: {},
+      settings: {
+        defaultFrom: 'skyluke011@gmail.com',
+      },
+    },
+  }
+}
